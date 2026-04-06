837d5204657dea8edf58fea0bce16d598ace1bca
audreysophia2010-max <audreysophia2010@gmail.com>
2026-04-06 21:12:14 +0000

    Add name prompt and date

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript here
+var input = prompt("What is your name?");
+input = input[0].toUpperCase() + input.slice(1).toLowerCase();
+alert("Welcome, " + input + "!");
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = new Date();
+
+var dayName = days[today.getDay()];
+var month = today.getMonth() + 1; 
+var day = today.getDate();
+var year = today.getFullYear();
+document.getElementById("date").textContent = (dayName + " " + day + + year);
